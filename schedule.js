c4641a5838772062f8bbd29bddd1e1f8cb68145d
bsk824 <bsk824@gmail.com>
2018-04-30 15:29:47 +0900

    test

--- schedule.js
+++ schedule.js
@@ -2,20 +2,15 @@ var sch = {
 	wrap : '.schedule_list',
 	dateWrap : '.date_sel',
 	timeWrap : '.time_sel',
-	padding : 213,
+	padding : 50,
 	scrollStatus: 0,
 	pos : [],
-	posH : [],
-	schDatePos : [],
-	schDateHPos : [],
 	datePos : [],
-	timePos : [],
-	timeDatePos : []
+	timePos : []
 }
 var posSet = function(ele) {
 	var listWrap = $('.' + ele),
 		date = listWrap.find('.date'),
-		dateH = listWrap.find('.date_wrap'),
 		items = listWrap.find('.item');
 	if(ele === 'date_sel') {
 		sch.datePos = [];
@@ -26,40 +21,19 @@ var posSet = function(ele) {
 		});
 	}
 	if(ele === 'time_sel') {
-		sch.timePos = [],
-		sch.timeDatePos = [];
+		sch.timePos = []
 		items.each(function(){
 			var $this = $(this),
 				pos = $this.position().top;
 			sch.timePos.push(pos);
 		});
-		date.each(function(){
-			var $this = $(this),
-				pos = $this.position().top;
-			sch.timeDatePos.push(pos);
-		});
 	}
 	if(ele === 'schedule_list') {
-		sch.Pos = [],
-		sch.schDatePos = [];
+		sch.pos = [];
 		items.each(function(){
 			var $this = $(this),
-				pos = $this.position().top,
-				posH = pos + $this.height();
-			
-			sch.pos.push(pos);
-			sch.posH.push(posH);
-		});
-		date.each(function(){
-			var $this = $(this),
 				pos = $this.position().top - sch.padding;
-			sch.schDatePos.push(pos);
-		});
-		dateH.each(function(){
-			var $this = $(this),
-				pos = $this.position().top - sch.padding,
-				posH = pos + $this.height();
-			sch.schDateHPos.push(posH);
+			sch.pos.push(pos);
 		});
 	}
 }
@@ -80,7 +54,7 @@ var dateActivePos = function(idx) {
 	var dateItemW = $(sch.dateWrap).find('.item').eq(idx).width() / 2,
 		dateCenter = dateItemW - ($(window).width() / 2);
 	function pos() {
-		return sch.datePos[idx] + dateCenter
+		return sch.datePos[idx] + dateCenter;
 	}
 	return pos();
 }
@@ -94,14 +68,12 @@ var dateSel = function(_this) {
 	$(sch.dateWrap).stop().animate({scrollLeft : dateActivePos(idx)},200);
 	
 	sch.scrollStatus = 1;
-	$(sch.wrap).stop().animate({scrollTop : sch.schDatePos[idx]},500);
-	$(sch.timeWrap).stop().animate({scrollTop : sch.timeDatePos[idx]},500,function(){
+	$(sch.wrap).stop().animate({scrollTop : sch.schPos[idx]},500);
+	$(sch.timeWrap).stop().animate({scrollTop : sch.timePos[idx]},500,function(){
 		sch.scrollStatus = 0;
 	});
 }
 var todayActive = function(idx) {
-	$(sch.wrap).scrollTop(sch.schDatePos[idx]);
-	$(sch.timeWrap).scrollTop(sch.timeDatePos[idx]);
 	$(sch.dateWrap).scrollLeft(dateActivePos(idx)).find('.item').eq(idx).removeClass('active');
 }
 
@@ -112,45 +84,30 @@ $(function(){
 	posSet('schedule_list');
 	
 	todayActive(7);
-	
-	$(sch.wrap).scroll(function(){
+
+	$(window).scroll(function(){
 		var $this = $(this),
 			scrollTop = $this.scrollTop(),
 			schItem = $(sch.wrap).find('.item'),
 			dateItem = $(sch.dateWrap).find('.item'),
 			timeItem = $(sch.timeWrap).find('.item'),
-      idx = 0;
+			idx = 0;
 		
-		// var idx = sch.posH.findIndex(function(item, idx) {
-		// 	return item - 200 >= scrollTop
-		// });
-		// var dateIdx = sch.schDateHPos.findIndex(function(item, idx) {
-		// 	return item - 200 > scrollTop
-		// });
-    sch.pos.some(function(val, now){
-      if (val <= scrollTop) {
-        idx = now;
-       }
-      
-    });
-    
-    console.log(idx, scrollTop, sch.pos[idx]);
-		
-		var activeDate = dateItem.eq(dateIdx),
-			activeItem = schItem.eq(idx),
+		sch.pos.some(function(val, now){
+			if (val <= scrollTop) {
+				idx = now;
+			}
+		});
+
+		var activeItem = schItem.eq(idx),
 			activeTime = timeItem.eq(idx);
 
 		activeItem.addClass('active').siblings().removeClass('active');
 		
-		if(!activeDate.hasClass('active') && sch.scrollStatus === 0) {
-			activeDate.addClass('active').siblings().removeClass('active');
-			$(sch.dateWrap).stop().animate({scrollLeft : dateActivePos(dateIdx)}, 200);
-		}
 		if(!activeTime.hasClass('active') && sch.scrollStatus === 0) {
 			activeTime.addClass('active').siblings().removeClass('active');
 			$(sch.timeWrap).stop().animate({scrollTop : sch.timePos[idx]},500);
 		}
 	});
 
-	
 });
